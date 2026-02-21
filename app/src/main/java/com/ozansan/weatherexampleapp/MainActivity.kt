ead3288fb7ba82de993b33f232bb6946fd7a04a0
ozan-san <ozan.san.98@gmail.com>
2026-02-21 21:14:21 +0300

    Add fonts

--- app/src/main/java/com/ozansan/weatherexampleapp/MainActivity.kt
+++ app/src/main/java/com/ozansan/weatherexampleapp/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ozansan.weatherexampleapp.ui.theme.WeatherExampleAppTheme
@@ -20,10 +23,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherExampleAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(
+                        Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
@@ -34,7 +42,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        style = MaterialTheme.typography.bodyMedium
     )
 }
 
--- app/src/main/java/com/ozansan/weatherexampleapp/ui/theme/Type.kt
+++ app/src/main/java/com/ozansan/weatherexampleapp/ui/theme/Type.kt
@@ -4,14 +4,34 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.ozansan.weatherexampleapp.R
 
-// Set of Material typography styles to start with
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val lobsterTwo = GoogleFont("VT323")
+
+val lobsterTwoFontFamily = FontFamily(
+    Font(googleFont = lobsterTwo, fontProvider = provider)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = lobsterTwoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 60.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
